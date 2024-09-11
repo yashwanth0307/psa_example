2e203de2d0cae5c01d852c99b584e64dc4451886
Yashwanth <practise2511@gmail.com>
2024-09-11 17:53:30 +0530

    defect fixed - 3568 - Fixed Null values not to be stored in database

--- src/main/java/com/git_example/A.java
+++ src/main/java/com/git_example/A.java
@@ -1,4 +1,7 @@
 package com.git_example;
 
 public class A {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
 }
---
+++ src/main/java/com/git_example/B.java
@@ -0,0 +1,7 @@
+package com.git_example;
+
+public class B {
+    public void test(){
+        System.out.println(100);
+    }
+}
