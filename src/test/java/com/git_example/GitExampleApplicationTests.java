84d4627d8f410ea01c2c6698f404cfb739838662
Yashwanth <practise2511@gmail.com>
2024-09-11 16:59:17 +0530

    Defect 4547 - Fixed the links which were not working in home page

---
+++ src/test/java/com/git_example/GitExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.git_example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
